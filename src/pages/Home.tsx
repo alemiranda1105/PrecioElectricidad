0deaca8e437d5221574da3aebf0bd41f3c087da0
Alejandro Miranda <amiranda110500@gmail.com>
2021-07-14 11:27:19 +0100

    Eliminada pantall fin de semana

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { IonCol, IonContent, IonGrid, IonHeader, IonToolbar, IonPage, IonRow, IonTitle } from '@ionic/react';
 import HourContainer from '../components/HourContainer';
-import ValleWeekend from '../components/tarifas/ValleWeekend';
 import TarifaContainer from '../components/tarifas/TarifaContainer';
 import PriceVisualizer from '../components/PriceVisualizer';
 import './Home.css';
@@ -34,37 +33,10 @@ const Home: React.FC = () => {
   if(day === "sábado" || day === "domingo") {
     console.log("fin de semana");
     color = "success";
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Precio de hoy ⚡</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent color={color}>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Precio de hoy ⚡</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonGrid>
-            <IonRow>
-              <IonCol>
-                <HourContainer />
-              </IonCol>
-            </IonRow>
-            <IonRow class="ion-align-items-center">
-              <IonCol sizeLg="6" sizeMd="12" sizeSm="12" sizeXs="12" size="12">
-                <ValleWeekend day={day}/>
-              </IonCol>
-              <IonCol sizeLg="6" sizeMd="12" sizeSm="12" sizeXs="12" size="12">
-                <PriceVisualizer />
-              </IonCol>
-            </IonRow>
-          </IonGrid>
-        </IonContent>
-      </IonPage>
-    );
+    description = "La más barata";
+    tarifa = "valle";
+    start = "sábado";
+    end = "domingo"
   } else if(isPunta(hour)) {
     color = "danger";
     description = "La más cara";
